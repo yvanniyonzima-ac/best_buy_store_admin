f3acc26df659ab9e5aa6b1284eac03d2d0604582
yvanniyonzima-ac <yvanniyonzima@Yvans-MacBook-Air.local>
2025-04-15 04:20:48 -0400

    Update best_buy_store_admin

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/best-buy.png" alt="Best Buy Store Logo">
       </a>
       Admin Portal
     </div>
